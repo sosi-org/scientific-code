daa3ec3cbcab58bee53510c715b5471cac729e4f
Sohail Siadatnejad <sohale@gmail.com>
2022-05-14 13:12:16 +0100

    copying code of the main plot from doulcier

--- timescales-state/fitzhugh-nagumo-model1.py
+++ timescales-state/fitzhugh-nagumo-model1.py
@@ -10,3 +10,57 @@ import scipy
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches #used to write custom legends
 # %matplotlib inline
+
+# Implement the flow of the Fitzhugh-Nagumo model.
+# And simulate some trajectories.
+# Try to use small perturbation of the resting potential as inital conditions.
+
+scenarios = [
+     {"a":-.3, "b":1.4, "tau":20, "I":0},
+     {"a":-.3, "b":1.4, "tau":20, "I":0.23},
+     {"a":-.3, "b":1.4, "tau":20, "I":0.5}
+]
+time_span = np.linspace(0, 200, num=1500)
+
+def fitzhugh_nagumo(x, t, a, b, tau, I):
+    """Time derivative of the Fitzhugh-Nagumo neural model.
+    Args:
+       x (array size 2): [Membrane potential, Recovery variable]
+       a, b (float): Parameters.
+       tau (float): Time scale.
+       t (float): Time (Not used: autonomous system)
+       I (float): Constant stimulus current.
+    Return: dx/dt (array size 2)
+    """
+    return np.array([x[0] - x[0]**3 - x[1] + I,
+                     (x[0] - a - b * x[1])/tau])
+def get_displacement(param, dmax=0.5,time_span=np.linspace(0,200, 1000), number=20):
+    # We start from the resting point...
+    ic = scipy.integrate.odeint(partial(fitzhugh_nagumo, **param),
+                                                      y0=[0,0],
+                                                      t= np.linspace(0,999, 1000))[-1]
+    # and do some displacement of the potential.
+    traj = []
+    for displacement in np.linspace(0,dmax, number):
+        traj.append(scipy.integrate.odeint(partial(fitzhugh_nagumo, **param),
+                                                      y0=ic+np.array([displacement,0]),
+                                                      t=time_span))
+    return traj
+
+# Do the numerical integration.
+trajectories = {} # We store the trajectories in a dictionnary, it is easier to recover them.
+for i,param in enumerate(scenarios):
+    trajectories[i] = get_displacement(param, number=3, time_span=time_span, dmax=0.5)
+
+# Draw the trajectories.
+fig, ax = plt.subplots(1, len(scenarios), figsize=(5*len(scenarios),5))
+for i,param in enumerate(scenarios):
+        ax[i].set(xlabel='Time', ylabel='v, w',
+                     title='{}'.format(param))
+        for j in range(len(trajectories[i])):
+            v = ax[i].plot(time_span,trajectories[i][j][:,0], color='C0')
+            w = ax[i].plot(time_span,trajectories[i][j][:,1], color='C1', alpha=.5)
+        ax[i].legend([v[0],w[0]],['v','w'])
+plt.tight_layout()
+
+plt.show()
