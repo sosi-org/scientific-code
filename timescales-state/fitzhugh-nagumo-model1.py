35ba04fc003cf0c279db1666b095f970f8ad2ebf
Sohail Siadatnejad <sohale@gmail.com>
2022-05-14 12:53:45 +0100

    staging the scene for FNM

---
+++ timescales-state/fitzhugh-nagumo-model1.py
@@ -0,0 +1,12 @@
+
+# variuos forms of FNM?
+
+# from wonderful page: https://www.normalesup.org/~doulcier/teaching/modeling/excitable_systems.html
+
+from functools import partial
+import numpy as np
+import scipy.integrate
+import scipy
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches #used to write custom legends
+# %matplotlib inline
---
+++ timescales-state/run-script.bash
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+# forked from https://github.com/sosi-org/primsoup/blob/master/actn/run-actn.bash
+
+set -xu
+
+source ~/cs/implisolid/scripts/bash-utils.sh
+
+function chk_virtualenv(){
+    # a solution based on `virutalenv`
+
+    #set -ex
+    if [[  -d ./p2-for-me ]]
+    then
+    # exists
+    return 0
+    #else
+    # does not exist
+    #return 0
+    fi
+
+    echo "INSTALLING THEM"
+
+    # brew install virtualenv
+
+    virtualenv -v --python=python2 p2-for-me
+    source ./p2-for-me/bin/activate
+    pip install numpy
+    pip install matplotlib
+}
+
+function chk_venv(){
+    # a solution based on `venv` as opposed to `virutalenv`
+
+    #set -ex
+    if [[  -d ./p3-for-me ]]
+    then
+    echo "venv exists"
+    return 0
+    fi
+
+    echo "INSTALLING THEM"
+    rm -rf p3-for-me || :
+
+    # venv is shipped with python3
+    #python3 -m venv -v --python=python3 p3-for-me
+    python3 -m venv p3-for-me
+    source ./p3-for-me/bin/activate
+
+    python --version
+    # Python 3.9.12
+
+    #pip install numpy
+    #pip install matplotlib
+
+    # python -m pip install \
+    #    --trusted-host files.pythonhosted.org \
+    #    --trusted-host pypi.org \
+    #    --trusted-host pypi.python.org \
+    #    [--proxy ...] [--user] <packagename>
+    #
+    #python -m pip install
+    #   --trusted-host files.pythonhosted.org \
+    #   --trusted-host pypi.org \
+    #   --trusted-host pypi.python.org --user \
+    #      numpy
+
+
+    # For trusted sources: see  https://stackoverflow.com/questions/49324802/pip-always-fails-ssl-verification
+
+    python -m \
+        pip install \
+            --trusted-host pypi.python.org \
+            --trusted-host files.pythonhosted.org \
+            --trusted-host pypi.org \
+            --upgrade pip
+
+    #python -m \
+        pip install \
+            --trusted-host pypi.python.org \
+            --trusted-host files.pythonhosted.org \
+            --trusted-host pypi.org \
+            numpy
+
+        pip install \
+            --trusted-host pypi.python.org \
+            --trusted-host files.pythonhosted.org \
+            --trusted-host pypi.org \
+            matplotlib
+}
+
+MAKE_HAPPEN "./p3-for-me/bin/activate" || {
+# chk_virtualenv
+chk_venv
+}
+
+echo "Main script"
+source ./p3-for-me/bin/activate
+
+python --version
+
+# python action1.py
+
+python action2.py
