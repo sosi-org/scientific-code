4a7447c32b778bc1438784f69e92933c5b4909f3
Sohail Siadatnejad <sohale@gmail.com>
2022-05-15 13:07:34 +0100

    code-hoard

--- timescales-state/nice_pieces_of_code.py
+++ timescales-state/nice_pieces_of_code.py
@@ -1,4 +1,5 @@
-
+# Please ignore this file.
+# It is a bunch of frgments of code for some reason.
 
 model_lamb = sympy.lambdify((*dyn_vars, *model_params, *model_inputs), dyn_derivs, dummify=False)
     #model_lamb = sympy.lambdify((*dyn_vars, *model_params, *model_inputs), dyn_derivs, dummify=False)
@@ -13,10 +14,11 @@ y[1].shape
 
 
 #nice
-
+'''
 import code
 code.interact(local=locals())
 exit()
+'''
 
 #import code
 #code.interact(local=locals())
@@ -40,3 +42,90 @@ model_lamb = sympy.lambdify((*dyn_vars,t), dyn_vars2, dummify=False)
 
           #np.format_float_positional(ranges[0][:], precision=3),
           #np.format_float_positional(ranges[1][:], precision=3),
+
+
+#sympy `solve` notes:
+# todo: why is v defined here?
+def symbolic_nullclines(_model, param):
+    # symbolic (algebraic) null-clines:
+    print()
+    # see https://github.com/sosi-org/scientific-code/blob/633224dc72692c13acc8263e21c933119eeaaf69/beeseyes/pycode/derive_formulas.py
+    dv = _model[2][0].subs(param)
+    dw = _model[2][1].subs(param)
+    w = _model[0][1]
+    v = _model[0][0]
+    #v = sympy.Symbol(v, real=True)
+    print('dv_', dv)
+    print('w', w)
+    from sympy import Eq #as Equation
+    # https://docs.sympy.org/latest/modules/core.html?highlight=eq#sympy.core.relational.Equality
+    # https://docs.sympy.org/latest/modules/solvers/solvers.html
+    if False:
+        zero = 0 * dv # ? sympy.core.numbers.Zero
+        eq1 = Eq(dv, zero)
+        sympy.pprint(eq1, use_unicode=True)
+        c = sympy.symbols("c")
+        eq2 = Eq(v, c*0, evaluate=False)
+        sympy.pprint(eq2, use_unicode=True)
+        # _.doit()
+        # simplify(_)
+        #solutions = sympy.solve(eq, (a,b,t),force=True, manual=True, set=True)
+        solutions = sympy.solve([eq1,eq2], (w,),force=True, manual=True, set=True)
+        print('solutions', solutions)  # ([w], {(-v**3 + v,)})
+        print('solutions[0]', solutions[0])
+        #sympy.core.numbers.Zero
+        #sol_a = solution[0][0]
+        #sol_b = solution[0][1]
+        #sol_t = solution[0][2]
+    if True:
+        from sympy import Matrix
+        from sympy import solve
+        c = sympy.symbols("c", real=True)
+        nlc1 = dv  # dv=0
+        #nlc1 = dw # dw=0
+        #lin1 = v-c # single root solution
+        lin1 = w-c  # multiple roots
+        print('nlc1', nlc1)
+        print('lin1', lin1)
+        eq3m = Matrix([nlc1, lin1])
+        zero0 =  Matrix([0,0])
+        #eq3 = Eq(matr, c*0, evaluate=False)
+        # unexplored option: evaluate=False
+        eq3 = Eq(eq3m, zero0)
+        sympy.pprint(eq3)
+        print('gonna solv')
+        #solutions = solve(eq3, (w,c),force=True, manual=True, set=True)
+        #solutions = solve(eq3, (w,),force=True, manual=True, set=True)
+        #vars_list, _solution_set = solutions = solve(eq3, (w,),force=True, set=True)
+        # ([w], {(-v**3 + v,)})
+        vars_list, _solution_set = solutions = solve(eq3, (v,w),force=True, set=True)
+        # convert set to list
+        print((vars_list))  # (v,w)
+        print((_solution_set))
+        print()
+        print(list(_solution_set))  # len = number of roots
+        solution_sympy_list = list(_solution_set)
+        # each solution is a tuple, for (v,w)
+        print(solution_sympy_list[0]) # has two elements
+        print(len(solution_sympy_list[0]))
+        print()
+        #solution_lambda_list = [
+        #    sympy.lambdify((c,), solt, dummify=False)
+        #        for solt in solution_sympy_list ]
+        # c is the free parameter for the "curve". Since it's aa null-cline, we need (exactly) one free ariable for it
+        # todo: rename c to s
+        solution_lambda_list = [
+            sympy.lambdify((c,), solt_vw, dummify=False)
+                for solt_vw in solution_sympy_list ]
+        # each is a function that returns ndim
+
+        return solution_sympy_list, solution_lambda_list
+
+
+        # without  `set=True`: returns list of solutions?
+        #[(-v**3 + v,)]
+
+    # solve: set=True:
+    # simplify=True
+    # "check=True (default) will run through the symbol tags to elimate unwanted solutions. If no assumptions are included, all possible solutions will be returned"
+    # why didi I choose this? "manual=True (default is False). Do not use the polys/matrix method to solve a system of equations, solve them one at a time as you might “manually.”"
