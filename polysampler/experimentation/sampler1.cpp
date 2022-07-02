96358a25429d933e53a3ed88540bac80fc8467a9
Sohail Siadatnejad <sohale@gmail.com>
2022-07-02 01:16:39 +0100

    pse1: reformat code

--- polysampler/experimentation/sampler1.cpp
+++ polysampler/experimentation/sampler1.cpp
@@ -13,53 +13,57 @@ typedef model::d2::point_xy point;
 #include <vector>
 
 // todo: int type
-typedef struct {double x; double y;} point_t;
+typedef struct
+{
+    double x;
+    double y;
+} point_t;
 // typedef struct {struct{int point_idx} main; struct {double a;} cache;} side_point;
 typedef int side_point_t; // FROM, and TO is the next one.
-typedef std::vector<std::vector<side_point_t> > triaglation_t;
+typedef std::vector<std::vector<side_point_t>> triaglation_t;
 // Also add meta-data to each item.
 typedef std::vector<point_t> points_t;
 // Delaunay triangulation
 
 points_t points = // {{1,2}, {3,4}};
-    {point_t{0,1},{0.4,0.8},{0.8, 0.5},{1,0.3},{0,-1},{-1,0},{-0.3,0.3}};
+    {point_t{0, 1}, {0.4, 0.8}, {0.8, 0.5}, {1, 0.3}, {0, -1}, {-1, 0}, {-0.3, 0.3}};
 
-triaglation_t trigulation = {{1,2,3,4,5}, {1,2,6}};
+triaglation_t trigulation = {{1, 2, 3, 4, 5}, {1, 2, 6}};
 
 // https://github.com/sosi-org/scientific-code/blob/main/beeseyes/pycode/polygon_sampler.py
-
 
 #include <iostream>
 #include <string>
 
-void do_side(const int &from_idx, const int &to_idx) {
+void do_side(const int &from_idx, const int &to_idx)
+{
     std::cout << from_idx << "-" << to_idx << ". ";
 }
 
-void traverse(const triaglation_t trigulation, const points_t points) {
-    for(auto plg = trigulation.begin(); plg < trigulation.end(); ++plg) {
+void traverse(const triaglation_t trigulation, const points_t points)
+{
+    for (auto plg = trigulation.begin(); plg < trigulation.end(); ++plg)
+    {
         const auto &polyg_i = *plg;
         std::cout << polyg_i.size() << ": ";
 
-        for(auto vert = polyg_i.begin(); vert < polyg_i.end()-1; ++vert) {
+        for (auto vert = polyg_i.begin(); vert < polyg_i.end() - 1; ++vert)
+        {
             const auto &from_i = *vert;
-            const auto &to_i = *(vert+1);
+            const auto &to_i = *(vert + 1);
             do_side(from_i, to_i);
-
         }
-        do_side(*(polyg_i.end()-1), *(polyg_i.begin()));
+        do_side(*(polyg_i.end() - 1), *(polyg_i.begin()));
 
         std::cout << std::endl;
     }
-
 }
 /*
     clang++ sampler1.cpp -std=c++2b
 */
 
-
-
-int main() {
+int main()
+{
     std::cout << "hi" << std::endl;
 
     traverse(trigulation, points);
