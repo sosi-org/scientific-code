9d2cabe313dffcf3acb9f0268195460b36d82393
Sohail Siadatnejad <sohale@gmail.com>
2022-07-08 11:42:38 +0100

    build script: refactoring (3) indentation

--- polysampler/experimentation/sampler1/build-20-all.bash
+++ polysampler/experimentation/sampler1/build-20-all.bash
@@ -4,6 +4,16 @@ FLAGS="-std=c++20 -stdlib=libc++ -fmodules -fbuiltin-module-map"
 
 cd /sosi/sampler1
 mkdir -p ./target
-clang++ $FLAGS -fprebuilt-module-path=./target -c   -Xclang -emit-module-interface  point_t.hpp -o ./target/point_t.module.precompiled
-clang++ $FLAGS -fprebuilt-module-path=./target -fmodule-file=point_t=./target/point_t.module.precompiled pmain.cpp -o ./target/a.out
+clang++ $FLAGS \
+    -fprebuilt-module-path=./target \
+    -c  \
+    -Xclang -emit-module-interface  \
+    point_t.hpp \
+    -o ./target/point_t.module.precompiled
+
+clang++ $FLAGS \
+    -fprebuilt-module-path=./target -fmodule-file=point_t=./target/point_t.module.precompiled \
+    pmain.cpp \
+    -o ./target/a.out
+
 date
