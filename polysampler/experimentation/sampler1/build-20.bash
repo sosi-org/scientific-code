5554fed034363f73c31a0dc43c3ea3fc7cabd413
Sohail Siadatnejad <sohale@gmail.com>
2022-07-08 10:05:27 +0000

    c++20 build outputs to ./target folder

--- polysampler/experimentation/sampler1/build-20.bash
+++ polysampler/experimentation/sampler1/build-20.bash
@@ -4,7 +4,8 @@
 FLAGS="-std=c++20 -stdlib=libc++ -fmodules -fbuiltin-module-map"
 docker run -it --rm -v $(realpath ..):/sosi -e FLAGS="$FLAGS"  conanio/clang14-ubuntu16.04:latest  bash -c '
 cd /sosi/sampler1;
-clang++ $FLAGS -fprebuilt-module-path=. -c   -Xclang -emit-module-interface  point_t.hpp -o point_t.module.precompiled;
-clang++ $FLAGS -fprebuilt-module-path=. -fmodule-file=point_t=point_t.module.precompiled pmain.cpp;
+mkdir -p ./target ;
+clang++ $FLAGS -fprebuilt-module-path=./target -c   -Xclang -emit-module-interface  point_t.hpp -o ./target/point_t.module.precompiled;
+clang++ $FLAGS -fprebuilt-module-path=./target -fmodule-file=point_t=./target/point_t.module.precompiled pmain.cpp -o ./target/a.out;
 date
 '
