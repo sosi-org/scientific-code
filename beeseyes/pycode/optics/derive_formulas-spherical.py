b088dbcc661c25d52cb11e4fdfc4a1aa5e3008a2
Sohail Siadatnejad <sohale@gmail.com>
2023-06-07 01:57:35 +0100

    the solutions. but not working correctly yet

--- beeseyes/pycode/optics/derive_formulas-spherical.py
+++ beeseyes/pycode/optics/derive_formulas-spherical.py
@@ -17,7 +17,7 @@ wx, wy, wz = symbols('wx wy wz', real=true)
 x0, y0, z0 = symbols('x0 y0 z0', real=true)
 
 
-a, b, c = symbols('a b c', real=true)
+uu, vv, ww = symbols('uu vv ww', real=true)
 
 various_forms = []
 # Part 1: UVW coordinates
@@ -37,14 +37,14 @@ UVW = Matrix([U.T, V.T, W.T]).T
 
 # Local cordinates:
 # todo: rename: uvw
-abc = Matrix([a,b,c])
+abc = Matrix([uu, vv, ww])
 
 point_in_sphere_uvw1 = UVW * abc + C0
 # point_in_sphere_uvw2 = norm(UVW * abc) - 1 #  = 0
 # pair them together?
 # No, it's just one equation:
 # implicit
-ellipsoid_surface_eq = a*a+b*b+c*c - 1
+ellipsoid_surface_eq = uu*uu + vv*vv + ww*ww - 1
 ellipsoid_surface_expl = point_in_sphere_uvw1
 
 # sympy.pprint(point_in_sphere_uvw1)
@@ -52,7 +52,7 @@ sympy.pprint(ellipsoid_surface_eq)
 # But this is not what we need
 
 
-variables = (a,b,c)
+variables = (uu,vv,ww)
 parameters = (U,V,W,C0)
 eq = ellipsoid_surface_eq
 # world_variables = (x,y,z) # no such thing
@@ -121,77 +121,52 @@ for form_i, form in enumerate(various_forms):
     eq0 = exeq - ray
     print('equation == 0 :')
     sympy.pprint(eq0)
+
+
+    # Equation
+    from sympy import Eq as Equation
+    # Solver
+
+    zero0 =  Matrix([0,0,0])
+    #eq = Equation(plane - ray, 0) # Does not work. Obviously.
+    eq = Equation(eq0, zero0)
+
+    # sympy.pprint(eq)
+    sympy.pprint(eq, use_unicode=True)
+
+    desired_vars = (uu,vv,ww, t)
+    # Solve!
+    solutions = solve(eq, desired_vars,force=True, manual=True, set=True)
+    solution = solve(eq, desired_vars,force=True, manual=True)
+
+    print('Num solutions: ', len(solution))
+    for solition_id in range(len(solution)):
+        print("Solution %d: has %d vars" % (solition_id, len(solution[solition_id])))
+        # sphere coords
+        sol_uu = solution[solition_id][0]
+        sol_vv = solution[solition_id][1]
+        sol_ww = solution[solition_id][2]
+        # ray coord(1)
+        sol_t = solution[solition_id][3]
+        # sol_abt = solution[solition_id]
+
+        print('raw: sol_u =', sol_uu )
+        print('raw: sol_v =', sol_vv )
+        print('raw: sol_w =', sol_ww )
+        print('raw: sol_t =', sol_t )
+
+        d1 = gcd(sol_uu, sol_vv)  # Did not work with `sol_t` (in Flat Plane)
+        d2 = gcd(d1, sol_ww)
+        d = d1
+        print('gcd = ', 1/d)
+
+        denom = 1/d
+        print('a =', sol_uu * denom  , '/', denom)
+        print('b =', sol_vv * denom  , '/', denom)
+        print('t =', sol_t * denom  , '/', denom)
+
 exit()
-'''
-⎡a⋅ux + b⋅vx - dx⋅t - ox⎤
-⎢                       ⎥
-⎢a⋅uy + b⋅vy - dy⋅t - oy⎥
-⎢                       ⎥
-⎣a⋅uz + b⋅vz - dz⋅t - oz⎦
-'''
 
-
-# Equation
-from sympy import Eq as Equation
-# Solver
-
-zero0 =  Matrix([0,0,0])
-#eq = Equation(plane - ray, 0) # Does not work. Obviously.
-eq = Equation(plane - ray, zero0)
-
-sympy.pprint(eq)
-'''
-⎡a⋅ux + b⋅vx - dx⋅t - ox⎤   ⎡0⎤
-⎢                       ⎥   ⎢ ⎥
-⎢a⋅uy + b⋅vy - dy⋅t - oy⎥ = ⎢0⎥
-⎢                       ⎥   ⎢ ⎥
-⎣a⋅uz + b⋅vz - dz⋅t - oz⎦   ⎣0⎦
-'''
-sympy.pprint(eq, use_unicode=True)
-
-
-#qq = solve(eq, (a,b,t))
-#qq = solve(eq, (t,a,b), set=True)
-
-#solve_linear_system(eq, t)
-#print(qq)
-
-#print(real_root(eq))
-#r = real_root(UV*ab - (O + t * D))
-#print('rrr', r)
-#sympy.pprint(r, use_unicode=True)
-
-solutions = solve(eq, (a,b,t),force=True, manual=True, set=True)
-#print(solutions)
-#([a, b, t],
-# {((dx*oy*vz - dx*oz*vy - dy*ox*vz + dy*oz*vx + dz*ox*vy - dz*oy*vx)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx), (-dx*oy*uz + dx*oz*uy + dy*ox*uz - dy*oz*ux - dz*ox*uy + dz*oy*ux)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx), (-ox*uy*vz + ox*uz*vy + oy*ux*vz - oy*uz*vx - oz*ux*vy + oz*uy*vx)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx))}
-# )
-
-#print(len(solutions))  # 2 ?!
-#print(solutions[0])  # [a, b, t]
-#print(solutions[1]) # {(a,b,t)}
-#(tupl, sol) = solutions
-
-solution = solve(eq, (a,b,t),force=True, manual=True)
-
-#print(solution) #[(,,)]
-#print(solution[0])
-# ((dx*oy*vz - dx*oz*vy - dy*ox*vz + dy*oz*vx + dz*ox*vy - dz*oy*vx)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx), (-dx*oy*uz + dx*oz*uy + dy*ox*uz - dy*oz*ux - dz*ox*uy + dz*oy*ux)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx), (-ox*uy*vz + ox*uz*vy + oy*ux*vz - oy*uz*vx - oz*ux*vy + oz*uy*vx)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx))
-
-
-sol_a = solution[0][0]
-sol_b = solution[0][1]
-sol_t = solution[0][2]
-
-# print(sol_a)
-# print(sol_b)
-# print(sol_t)
-# (dx*oy*vz - dx*oz*vy + dx*vy*z0 - dx*vz*y0 - dy*ox*vz + dy*oz*vx - dy*vx*z0 + dy*vz*x0 + dz*ox*vy - dz*oy*vx + dz*vx*y0 - dz*vy*x0)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx)
-# (-dx*oy*uz + dx*oz*uy - dx*uy*z0 + dx*uz*y0 + dy*ox*uz - dy*oz*ux + dy*ux*z0 - dy*uz*x0 - dz*ox*uy + dz*oy*ux - dz*ux*y0 + dz*uy*x0)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx)
-# (-ox*uy*vz + ox*uz*vy + oy*ux*vz - oy*uz*vx - oz*ux*vy + oz*uy*vx + ux*vy*z0 - ux*vz*y0 - uy*vx*z0 + uy*vz*x0 + uz*vx*y0 - uz*vy*x0)/(dx*uy*vz - dx*uz*vy - dy*ux*vz + dy*uz*vx + dz*ux*vy - dz*uy*vx)
-
-d = gcd(sol_a, sol_b)  # Does not work with `sol_t`
-print('gcd = ', 1/d)
 
 denom = 1/d
 print('a =', sol_a * denom  , '/', denom)
