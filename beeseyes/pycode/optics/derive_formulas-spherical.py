893df8855bf9a75eafc7aff43b1f3f68174220f1
Sohail Siadatnejad <sohale@gmail.com>
2023-06-08 13:17:06 +0100

    lint: big

--- beeseyes/pycode/optics/derive_formulas-spherical.py
+++ beeseyes/pycode/optics/derive_formulas-spherical.py
@@ -1,9 +1,9 @@
 import sympy
 
 # This program generates automatic c-code for gradient of
-#implicit objects with complicated formulas. It generates the gradient automatically.
+# implicit objects with complicated formulas. It generates the gradient automatically.
 
-#from __future__ import division
+# from __future__ import division
 import sympy
 from sympy import *
 
@@ -17,23 +17,21 @@ def surface_sphere_polar():
     wx, wy, wz = symbols('wx wy wz', real=true)
     x0, y0, z0 = symbols('x0 y0 z0', real=true)
 
-
     uu, vv, ww = symbols('uu vv ww', real=true)
 
     # Part 1: UVW coordinates
 
     # Sphere parameters:
     # Center
-    C0 = Matrix([x0,y0,z0])
+    C0 = Matrix([x0, y0, z0])
     # Plane: side vectors
-    U = Matrix([ux,uy,uz])
-    V = Matrix([vx,vy,vz])
-    W = Matrix([wx,wy,wz])
+    U = Matrix([ux, uy, uz])
+    V = Matrix([vx, vy, vz])
+    W = Matrix([wx, wy, wz])
     # UV = Matrix([U.T, V.T]).T
     UVW = Matrix([U.T, V.T, W.T]).T
 
     # I wish I had SDF. That would be automatically geodesic.
-
 
     # Local cordinates:
     # todo: rename: uvw
@@ -51,32 +49,33 @@ def surface_sphere_polar():
     sympy.pprint(ellipsoid_surface_eq)
     # But this is not what we need
 
-
-    variables = (uu,vv,ww)
-    parameters = (U,V,W,C0)
+    variables = (uu, vv, ww)
+    parameters = (U, V, W, C0)
     eq = ellipsoid_surface_eq
     # world_variables = (x,y,z) # no such thing
 
     return {
-        'vars': variables, # local vars
+        'vars': variables,  # local vars
         # 'x,y,z': world_variables, # local vars ---> 'expl-eq'
         'params': parameters,
         'impl-eq': eq,
         'expl-eq': ellipsoid_surface_expl,
-        'unknowns': (uu,vv,ww),
-        'desired_vars': [uu,vv,ww],
+        'unknowns': (uu, vv, ww),
+        'desired_vars': [uu, vv, ww],
     }
+
 
 various_forms = []
 various_forms.append(surface_sphere_polar())
+
 
 def surface_sphere_polar():
     # Part 2: Polar coordinates
     # Formulation 2:
 
-    φ,θ,r = symbols('φ θ r', real=true)
+    φ, θ, r = symbols('φ θ r', real=true)
     x0, y0, z0 = symbols('x0 y0 z0', real=true)
-    C0 = Matrix([x0,y0,z0])
+    C0 = Matrix([x0, y0, z0])
 
     x = r * sin(φ) * cos(θ) + x0
     y = r * sin(φ) * sin(θ) + y0
@@ -90,16 +89,17 @@ def surface_sphere_polar():
     polar_sphere_eq = x*x + y*y + z*z - r*r
 
     return {
-        'vars': (φ,θ,r),
+        'vars': (φ, θ, r),
         'params': (C0),
         # Implicit. Constraints. In fugure, it can be more than one
         'impl-eq': polar_sphere_eq,
         # Explicit
         # Explicit: Cartesian (x,y,z) (in host space: aug-dims representation) (vector3D)
-        'expl-eq': Matrix([x,y,z]),
-        'unknowns': (φ,θ,r),
-        'desired_vars': [φ,θ,r],
+        'expl-eq': Matrix([x, y, z]),
+        'unknowns': (φ, θ, r),
+        'desired_vars': [φ, θ, r],
     }
+
 
 various_forms.append(surface_sphere_polar())
 
@@ -108,14 +108,14 @@ various_forms.append(surface_sphere_polar())
 # ------------------------------
 # Now  ray (to cross with each):
 
-# Ray:
+#  Ray:
 ox, oy, oz = symbols('ox oy oz', real=true)
 dx, dy, dz = symbols('dx dy dz', real=true)
 
 # Ray origin:
-O = Matrix([ox,oy,oz])
+O = Matrix([ox, oy, oz])
 # Ray direction:
-D = Matrix([dx,dy,dz])
+D = Matrix([dx, dy, dz])
 # Ray drive (t,l)
 t = symbols('t', real=true)
 
@@ -139,73 +139,72 @@ ray_thing = {
 
 chosen_forms = [various_forms[1]]
 for form_i, form in enumerate(chosen_forms):
-  # set of desired vars: desired_set, unkowns, fast_vars, vars, surface_vars
-  # May be any number of DoF: Local DoF. (Usually DoF equals to DoF of host space): (u,v,w) or (φ,θ,r)
-  surface_vars = form['desired_vars']
-  # Only (t,)  (DoF = 1)
-  ray_vars = ray_thing['desired_vars']
-  #desired_set_tuple = (*surface_vars, *ray_vars)
-  #print('desired_set', desired_set)
-  desired_vars_tuple = (*surface_vars, *ray_vars)
-  print('desired_vars_tuple', desired_vars_tuple)
+    # set of desired vars: desired_set, unkowns, fast_vars, vars, surface_vars
+    # May be any number of DoF: Local DoF. (Usually DoF equals to DoF of host space): (u,v,w) or (φ,θ,r)
+    surface_vars = form['desired_vars']
+    # Only (t,)  (DoF = 1)
+    ray_vars = ray_thing['desired_vars']
+    # desired_set_tuple = (*surface_vars, *ray_vars)
+    # print('desired_set', desired_set)
+    desired_vars_tuple = (*surface_vars, *ray_vars)
+    print('desired_vars_tuple', desired_vars_tuple)
 
-  #for desiredv_j, desired_var in enumerate(desired_set):
-  if True:
-    vars = form['vars']
-    params = form['params']
-    # Implicit equation for each: f(...) = 0
-    # eq = form['eq']
-    ieq = form['impl-eq']
-    # Explicit equation for each: (x,y,z) := ...  host_eq
-    exeq = form['expl-eq']
-    rayeq = ray_thing['expl-eq']
-    eq0 = exeq - rayeq
-    print('equation == 0 :')
-    sympy.pprint(eq0)
-    # desired_vars_tuple = (desired_var,)
+    # for desiredv_j, desired_var in enumerate(desired_set):
+    if True:
+        vars = form['vars']
+        params = form['params']
+        # Implicit equation for each: f(...) = 0
+        # eq = form['eq']
+        ieq = form['impl-eq']
+        # Explicit equation for each: (x,y,z) := ...  host_eq
+        exeq = form['expl-eq']
+        rayeq = ray_thing['expl-eq']
+        eq0 = exeq - rayeq
+        print('equation == 0 :')
+        sympy.pprint(eq0)
+        # desired_vars_tuple = (desired_var,)
 
+        # Equation
+        from sympy import Eq as Equation
+        # Solver
 
-    # Equation
-    from sympy import Eq as Equation
-    # Solver
+        zero0 = Matrix([0, 0, 0])
+        # eq = Equation(plane - ray, 0) # Does not work. Obviously.
+        # eq = Equation(eq0, zero0)
+        # Unike the old one, we have two equaiotns: Since the plat plane is not enough (one equation), but we increase diention,a dn the constain it.
+        # ieq = constrain
+        # exeq - ray = higher-dimension repr + clams
+        # eq = Equation((exeq - ray, ieq), zero0)
+        eq1 = Equation(exeq - ray, zero0)
+        eq2 = Equation(ieq, 0)
+        # 3 (higher-dim) + 1 (scalar)
+        # Later on, add Symplectic: add to 36, and add constraints.
 
-    zero0 =  Matrix([0,0,0])
-    #eq = Equation(plane - ray, 0) # Does not work. Obviously.
-    # eq = Equation(eq0, zero0)
-    # Unike the old one, we have two equaiotns: Since the plat plane is not enough (one equation), but we increase diention,a dn the constain it.
-    # ieq = constrain
-    # exeq - ray = higher-dimension repr + clams
-    # eq = Equation((exeq - ray, ieq), zero0)
-    eq1 = Equation(exeq - ray, zero0)
-    eq2 = Equation(ieq, 0)
-    # 3 (higher-dim) + 1 (scalar)
-    # Later on, add Symplectic: add to 36, and add constraints.
+        print('Equation2 to solve:')
+        sympy.pprint(eq1, use_unicode=True)
+        sympy.pprint(eq2, use_unicode=True)
 
-    print('Equation2 to solve:')
-    sympy.pprint(eq1, use_unicode=True)
-    sympy.pprint(eq2, use_unicode=True)
+        # desired_vars = (uu,vv,ww, t)
+        # If I don't take all, it wil consider the other desired variables as knowns
+        # all unkonwns need to be in one place
+        # uknowns = ,
+        print('To solve eq. set for', desired_vars_tuple)
+        if False:
+            # fake the solution:
+            solution = [desired_vars_tuple]
+        # Solve!
+        if False:
+            solutions = solve((eq1, eq2), desired_vars_tuple,
+                              force=True, manual=True, set=True)
+        solution = solve((eq1, eq2), desired_vars_tuple,
+                         force=True, manual=True)
 
-
-    # desired_vars = (uu,vv,ww, t)
-    # If I don't take all, it wil consider the other desired variables as knowns
-    # all unkonwns need to be in one place
-    # uknowns = ,
-    print('To solve eq. set for', desired_vars_tuple)
-    if False:
-      # fake the solution:
-      solution = [desired_vars_tuple]
-    # Solve!
-    if False:
-      solutions = solve((eq1,eq2), desired_vars_tuple, force=True, manual=True, set=True)
-    solution = solve((eq1,eq2), desired_vars_tuple, force=True, manual=True)
-
-
-    print('Num solutions: ', len(solution))
-    solutions = []
-    for solition_id in range(len(solution)):
-        num_desired_vars = len(solution[solition_id])
-        print("Solution %d: has %d vars" % (solition_id, num_desired_vars))
-        """
+        print('Num solutions: ', len(solution))
+        solutions = []
+        for solition_id in range(len(solution)):
+            num_desired_vars = len(solution[solition_id])
+            print("Solution %d: has %d vars" % (solition_id, num_desired_vars))
+            """
         # sphere coords
         sol_uu = solution[solition_id][0]
         sol_vv = solution[solition_id][1]
@@ -214,44 +213,45 @@ for form_i, form in enumerate(chosen_forms):
         sol_t = solution[solition_id][3]
         # sol_abt = solution[solition_id]
         """
-        for j in range(num_desired_vars):
-          varname = str(desired_vars_tuple[j])
-          # Algebraic (symbolic) solution
-          sol_v = solution[solition_id][j]
+            for j in range(num_desired_vars):
+                varname = str(desired_vars_tuple[j])
+                # Algebraic (symbolic) solution
+                sol_v = solution[solition_id][j]
 
-          """
+                """
           print('raw: sol_u =', sol_uu )
           print('raw: sol_v =', sol_vv )
           print('raw: sol_w =', sol_ww )
           print('raw: sol_t =', sol_t )
           """
-          print(f'raw: sol_{varname} =', sol_v )
-          solutions += [sol_v]
-    print('Calculating gcd, for speed')
-    if False:
-        d1 = gcd(sol_uu, sol_vv)  # Did not work with `sol_t` (in Flat Plane)
-        d2 = gcd(d1, sol_ww)
-        print('d1 = ', d1)
-        print('d2 = ', d2)
-        d = d1
-        print('gcd = ', 1/d)
+                print(f'raw: sol_{varname} =', sol_v)
+                solutions += [sol_v]
+        print('Calculating gcd, for speed')
+        if False:
+            # Did not work with `sol_t` (in Flat Plane)
+            d1 = gcd(sol_uu, sol_vv)
+            d2 = gcd(d1, sol_ww)
+            print('d1 = ', d1)
+            print('d2 = ', d2)
+            d = d1
+            print('gcd = ', 1/d)
 
-        denom = 1/d
-        print('a =', sol_uu * denom  , '/', denom)
-        print('b =', sol_vv * denom  , '/', denom)
-        print('t =', sol_t * denom  , '/', denom)
-    print('--------------------')
-    # It is slow. How to save the result when done?
+            denom = 1/d
+            print('a =', sol_uu * denom, '/', denom)
+            print('b =', sol_vv * denom, '/', denom)
+            print('t =', sol_t * denom, '/', denom)
+        print('--------------------')
+        # It is slow. How to save the result when done?
 
-    # todo: save/replace LaTeX for mapltotlib
+        # todo: save/replace LaTeX for mapltotlib
 
 exit()
 
 
 denom = 1/d
-print('a =', sol_a * denom  , '/', denom)
-print('b =', sol_b * denom  , '/', denom)
-print('t =', sol_t * denom  , '/', denom)
+print('a =', sol_a * denom, '/', denom)
+print('b =', sol_b * denom, '/', denom)
+print('t =', sol_t * denom, '/', denom)
 
 '''
 Outcome:
